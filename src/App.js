408e3aac26a0d488024f706d6e24e4cbe59cbb49
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-31 12:13:37 +0100

    ein react übungsprojekt; erster commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <div className='introBox'>
+        <div className='logoMarge'>
+          <img src="./logo.png" className="App-logo" alt="logo" />
+       </div>		    
+      </div>
+
+<h2>Hello, my name is Enza.</h2>
+
+      <div className='flipCardBoxBox'>        
+      <div className="flip-card">
+          <div className="flip-card-inner">
+
+            <div className="flip-card-front">
+              <img className='enzaBlu' src='./enzaRotundeGree4_500sheinB.jpg' alt='enza in rotunde auf alsen blu' />
+            </div>
+
+            <div className="flip-card-back">
+
+              <h1>enza &#8226; `22 inTheBackyard</h1> 
+
+              <div className='txt2Box'>
+                <p className='txt2'>Artist & bEgInner</p> 
+                <p className='txt3'>~We love that guy~</p>
+                <h1>&#9775;</h1>
+                <p className='txt4'>The first, that is wanted, is LOVE .. but no wait .. the first is a dry roof above the head and water.. (somehow .. fresh or not). hmmm ...</p>
+                <p className='txt4'>
+                  WHY .. don´t you look outside to proof, what you are telled?
+                </p>
+                <p>FEEL FREE to PRACTISE.</p>
+              </div> 
+
+            </div>
+            
+          </div>
+      </div>
+      </div>
+
+    <div className='someTimesBoxBox'>
+      <div className='someTimesBox'>
+          <p className='someTimes'>Sometimes life seems to be like prison or punishment, but in fact, all situations and experiences serve our completely individual development towards a "better version of ourselves".</p>
+              <div className='phiBox'>
+                  <p className='phi'>&#934; &#934; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#934; &#934;</p>
+              </div>
+        </div>
+    </div>
+
+   
+
+<div className='img2txtBox'>
+  <img className='img2' src='./enzaRotundeBlu2_500sheinVerbl.jpg'/>
+  <img className='img2' src='./enzaRotundeYell3_500.jpg'/> 
+  <img className='img2' src='./enzaAlsenRotundeBlu_500.jpg'/>
+  <img className='img2' src='./enzaRotundeSkull5_500.jpg'/>
+  
+    <p className='img2txt'>photos: Olaf Plotz</p></div>
+
+              <div className='phiBox'>
+                  <p className='phi'>&#934; &#934; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#934; &#934; &#8226; &#934; &#934; &#934;</p>
+              </div>
+
+
+    <div className="card">
+      <img src="./enzaAlsenCard300.jpg" alt="Enza" />
+      <h1 className='evm'>Enza von metaTronics</h1>
+      <p className="title">&#8226; CEO & Co-Founder &#8226; metaTronics&#174;2008-22</p>
+      <p className='dci'>~ DCI-student ~</p>
+
+      <div className='links'>
+        <a href="#"><i className="fa fa-home"></i></a>
+        <a href="https://www.youtube.com/channel/UCk0h-WIIayMkBjSZ161frNA" target={"_blank"}><i className="fa fa-youtube"></i></a>
+        <a href="https://soundcloud.com/enzara" target={"_blank"}><i className="fa fa-soundcloud"></i></a>
+        <a href="https://www.facebook.com/enza.ra.58/" target={"_blank"}><i className="fa fa-facebook"></i></a>
+      </div>
+
+      <a href='claudi.enza@outlook.de' target={"_blank"}>
+        <p><button>Contact</button></p>
+      </a>
+   </div>
+
+
+<div className='yt-qrBox'>
+  <img className='yt-qr' src='./qrcode_www.youtube.com.png' alt='qr-code youtube-site' />
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
